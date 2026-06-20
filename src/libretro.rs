c5a023a265ef4281540be05e86d8dfa4114d51b9
David Brinovec <david.brinovec@gmail.com>
2026-06-19 21:48:17 -0400

    memory_requested debugging updated

--- src/libretro.rs
+++ src/libretro.rs
@@ -425,6 +425,7 @@ pub extern "C" fn retro_run() {
         eprintln!("bytes at $022C0 .. $022D9: {:02x?}", &core.machine.z80.io.mem[0x22C0..=0x22D9]);
         eprintln!("byte at $04F3C: {:02x?}", &core.machine.z80.io.mem[0x4F3C]);
         eprintln!("bytes at $02AF5 .. $02B20: {:02x?}", &core.machine.z80.io.mem[0x2AF5..=0x2B20]);
+        eprintln!("byte at $04F27: {:02x?}", &core.machine.z80.io.mem[0x4F27]);
     }
 
     // Machine
