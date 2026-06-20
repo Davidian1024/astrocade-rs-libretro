d0a35303ba0fab3a7300b2739b3b1789c5d4de2d
David Brinovec <david.brinovec@gmail.com>
2026-06-19 21:58:34 -0400

    watch added

--- src/libretro.rs
+++ src/libretro.rs
@@ -495,33 +495,33 @@ pub extern "C" fn retro_run() {
             irq_asserted = true;
             last_irq_fire_x4 = Some(irq_fire_cycle_x4);
             
-            #[cfg(feature = "debug_logging")]
-            debug_print!(
-                core.step_count,
-                core.frame_count,
-                frame_step_x4 / 4,
-                "I pc={:#06x}:{:12} inmod={:#04x} inlin={:#04x} infbk={:#04x} irq_enabled={:5} $4FCE={:#06x} $4FD0={:#06x} $4FD4={:#04x} $4FEA={:#04x} $4FF9={:#04x} scanline={:#03} {}{}",
-                core.machine.z80.pc,
-                disassemble_at(&core.machine.z80.io.mem, core.machine.z80.pc),
-                core.machine.z80.io.inmod,
-                core.machine.z80.io.inlin,
-                core.machine.z80.io.infbk,
-                (core.machine.z80.io.inmod & 0x08) != 0,
-                u16::from_le_bytes([
-                    core.machine.z80.io.mem[0x4FCE],
-                    core.machine.z80.io.mem[0x4FCF]
-                ]),
-                u16::from_le_bytes([
-                    core.machine.z80.io.mem[0x4FD0],
-                    core.machine.z80.io.mem[0x4FD1]
-                ]),
-                core.machine.z80.io.mem[0x4FD4],
-                core.machine.z80.io.mem[0x4FEA],
-                core.machine.z80.io.mem[0x4FF9],
-                irq_physical_line,
-                if catch_trigger { "Trigger" } else { "" },
-                if catch_enter { "Enter" } else { "" },
-            );
+            // #[cfg(feature = "debug_logging")]
+            // debug_print!(
+            //     core.step_count,
+            //     core.frame_count,
+            //     frame_step_x4 / 4,
+            //     "I pc={:#06x}:{:12} inmod={:#04x} inlin={:#04x} infbk={:#04x} irq_enabled={:5} $4FCE={:#06x} $4FD0={:#06x} $4FD4={:#04x} $4FEA={:#04x} $4FF9={:#04x} scanline={:#03} {}{}",
+            //     core.machine.z80.pc,
+            //     disassemble_at(&core.machine.z80.io.mem, core.machine.z80.pc),
+            //     core.machine.z80.io.inmod,
+            //     core.machine.z80.io.inlin,
+            //     core.machine.z80.io.infbk,
+            //     (core.machine.z80.io.inmod & 0x08) != 0,
+            //     u16::from_le_bytes([
+            //         core.machine.z80.io.mem[0x4FCE],
+            //         core.machine.z80.io.mem[0x4FCF]
+            //     ]),
+            //     u16::from_le_bytes([
+            //         core.machine.z80.io.mem[0x4FD0],
+            //         core.machine.z80.io.mem[0x4FD1]
+            //     ]),
+            //     core.machine.z80.io.mem[0x4FD4],
+            //     core.machine.z80.io.mem[0x4FEA],
+            //     core.machine.z80.io.mem[0x4FF9],
+            //     irq_physical_line,
+            //     if catch_trigger { "Trigger" } else { "" },
+            //     if catch_enter { "Enter" } else { "" },
+            // );
         }
 
         // DEBUG: Per-second state dump
@@ -563,7 +563,25 @@ pub extern "C" fn retro_run() {
         let current_inmod = core.machine.z80.io.inmod;
         let current_inlin = core.machine.z80.io.inlin;
         let current_infbk = core.machine.z80.io.infbk;
+
+        let prev_4f27 = core.machine.z80.io.mem[0x4F27];
         let cycles = core.machine.z80.step();
+        core.step_count += 1;
+        frame_step_x4 += cycles * 4;
+
+        #[cfg(feature = "debug_logging")]
+        if core.machine.z80.io.mem[0x4F27] != prev_4f27 {
+            eprintln!(
+                "WATCH $4F27: {:02x} -> {:02x} at pc={:#06x} step={} frame={} fstep={}",
+                prev_4f27,
+                core.machine.z80.io.mem[0x4F27],
+                core.machine.z80.pc,
+                core.step_count,
+                core.frame_count,
+                frame_step_x4 / 4,
+            );
+        }
+
         core.step_count += 1;
         frame_step_x4 += cycles * 4;
 
