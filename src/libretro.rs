6adad25745a3bb88ea5fd6beefc5d437c8edfc8a
David Brinovec <david.brinovec@gmail.com>
2026-06-19 21:05:19 -0400

    pushing latest

--- src/libretro.rs
+++ src/libretro.rs
@@ -472,7 +472,10 @@ pub extern "C" fn retro_run() {
             last_irq_fire_x4 = Some(irq_fire_cycle_x4);
         }
 
-        if scanline_irq_enabled && is_new_threshold && frame_step_x4 >= irq_fire_cycle_x4 {
+        if scanline_irq_enabled
+            && is_new_threshold
+            && frame_step_x4 >= irq_fire_cycle_x4
+        {
             core.machine.z80.assert_irq(core.machine.z80.io.infbk);
             irq_asserted = true;
             last_irq_fire_x4 = Some(irq_fire_cycle_x4);
@@ -482,7 +485,7 @@ pub extern "C" fn retro_run() {
                 core.step_count,
                 core.frame_count,
                 frame_step_x4 / 4,
-                "I pc={:#06x}:{:12} inmod={:#04x} inlin={:#04x} infbk={:#04x} irq_enabled={:5} $4FCE={:#06x} $4FD0={:#06x} $4FD4={:#04x} $4FEA={:#04x} $4FF9={:#04x} scanline={} {}{}",
+                "I pc={:#06x}:{:12} inmod={:#04x} inlin={:#04x} infbk={:#04x} irq_enabled={:5} $4FCE={:#06x} $4FD0={:#06x} $4FD4={:#04x} $4FEA={:#04x} $4FF9={:#04x} scanline={:#03} {}{}",
                 core.machine.z80.pc,
                 disassemble_at(&core.machine.z80.io.mem, core.machine.z80.pc),
                 core.machine.z80.io.inmod,
@@ -513,7 +516,7 @@ pub extern "C" fn retro_run() {
                 core.step_count,
                 core.frame_count,
                 frame_step_x4 / 4,
-                "> pc={:#06x}:{:12} inmod={:#04x} inlin={:#04x} infbk={:#04x} irq_enabled={:5} $4FCE={:#06x} $4FD0={:#06x} $4FD4={:#04x} $4FEA={:#04x} $4FF9={:#04x} {}{}",
+                "> pc={:#06x}:{:12} inmod={:#04x} inlin={:#04x} infbk={:#04x} irq_enabled={:5} $4FCE={:#06x} $4FD0={:#06x} $4FD4={:#04x} $4FEA={:#04x} $4FF9={:#04x} scanline={:#03} {}{}",
                 core.machine.z80.pc,
                 disassemble_at(&core.machine.z80.io.mem, core.machine.z80.pc),
                 core.machine.z80.io.inmod,
@@ -531,6 +534,7 @@ pub extern "C" fn retro_run() {
                 core.machine.z80.io.mem[0x4FD4],
                 core.machine.z80.io.mem[0x4FEA],
                 core.machine.z80.io.mem[0x4FF9],
+                irq_physical_line,
                 if catch_trigger { "Trigger" } else { "" },
                 if catch_enter { "Enter" } else { "" },
             );
@@ -555,7 +559,7 @@ pub extern "C" fn retro_run() {
                 core.step_count,
                 core.frame_count,
                 frame_step_x4 / 4,
-                "» pc={:#06x}:{:12} inmod={:#04x} inlin={:#04x} infbk={:#04x} irq_enabled={:5} $4FCE={:#06x} $4FD0={:#06x} $4FD4={:#04x} $4FEA={:#04x} $4FF9={:#04x} {}{}",
+                "» pc={:#06x}:{:12} inmod={:#04x} inlin={:#04x} infbk={:#04x} irq_enabled={:5} $4FCE={:#06x} $4FD0={:#06x} $4FD4={:#04x} $4FEA={:#04x} $4FF9={:#04x} scanline={:#03} {}{}",
                 core.machine.z80.pc,
                 disassemble_at(&core.machine.z80.io.mem, core.machine.z80.pc),
                 core.machine.z80.io.inmod,
@@ -573,6 +577,7 @@ pub extern "C" fn retro_run() {
                 core.machine.z80.io.mem[0x4FD4],
                 core.machine.z80.io.mem[0x4FEA],
                 core.machine.z80.io.mem[0x4FF9],
+                irq_physical_line,
                 if catch_trigger { "Trigger" } else { "" },
                 if catch_enter { "Enter" } else { "" },
             );
